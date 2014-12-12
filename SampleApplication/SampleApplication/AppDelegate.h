bc5275186ba8bfd6b907b3ebde95303517f7974f
Sudheesh P <sudheesh@Sudheeshs-iMac.local>
2014-12-12 10:43:35 +0530

    Initial commit

---
+++ SampleApplication/SampleApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleApplication
+//
+//  Created by Sudheesh P on 12/12/14.
+//  Copyright (c) 2014 qburst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleApplication/SampleApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApplication
+//
+//  Created by Sudheesh P on 12/12/14.
+//  Copyright (c) 2014 qburst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
